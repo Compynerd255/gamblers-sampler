17eb216b277d4e4e8f6304cb19cd12f3878b82fa
Hans Jorgensen <hans@thehansinator.net>
2017-12-27 13:07:29 -0800

    Added sample program

--- Betafreak.GamblersSampler/GamblersSamplerExamples/Program.cs
+++ Betafreak.GamblersSampler/GamblersSamplerExamples/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Betafreak.GamblersSampler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,28 @@ namespace GamblersSamplerExamples
     {
         static void Main(string[] args)
         {
+            ISampler<int> uniform = Samplers.UniformD20();
+            Console.WriteLine("Uniform Sample:");
+            Simulate80PercentXCOMShots(uniform);
+
+            ISampler<int> gamblers = Samplers.GamblersD20();
+            Console.WriteLine("Gambler's Sample:");
+            Simulate80PercentXCOMShots(gamblers);
+
+            Console.ReadLine();
+            // Simulate a set of 20 
+        }
+
+        static void Simulate80PercentXCOMShots(ISampler<int> d20)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < Console.WindowWidth; j++)
+                {
+                    Console.Write(d20.Next() > 16 ? '*' : '_');
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
